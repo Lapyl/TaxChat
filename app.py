878601101c7011f4f533c20a0c435b59e8439230
Lalit A Patel <Lapyl@users.noreply.github.com>
2023-07-27 21:19:18 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
+import streamlit as st
 from llama_index import StorageContext, load_index_from_storage
 import gradio as gr
+"""
+# TaxChat
+### @ Streamlit GitHub Lapyl
+- [GitHub Repository](https://github.com/Lapyl/Kaleido)
+- [Streamlit Dcumentation](https://docs.streamlit.io)
+- [Streamlit Forums](https://discuss.streamlit.io)
+"""
 
-def chatbot(zask="Where is Florida?"):
-    storage_context = StorageContext.from_defaults(persist_dir='Meta_Index\FloridaSutLaws')
-    index = load_index_from_storage(storage_context)
-    query_engine = index.as_query_engine()
-    response = query_engine.query(zask)
-    return response
-
-iface = gr.Interface(fn=chatbot,
-                     inputs=gr.components.Textbox(lines=7, label="Input Text"),
-                     outputs=gr.components.Textbox(lines=7, label="Output Text"),
-                     title="Chatbot regarding Florida Sales & Use Tax Statutes and Codes")
-
-iface.launch(share=True)
-
+with st.echo(code_location='below'):
+    def chatbot(zask="Where is Florida?"):
+        storage_context = StorageContext.from_defaults(persist_dir='Meta_Index/FloridaSutLaws')
+        index = load_index_from_storage(storage_context)
+        query_engine = index.as_query_engine()
+        response = query_engine.query(zask)
+        return response
+    iface = gr.Interface(fn=chatbot,
+            inputs=gr.components.Textbox(lines=7, label="Input Text"),
+            outputs=gr.components.Textbox(lines=7, label="Output Text"),
+            title="Chatbot regarding Florida Sales & Use Tax Statutes and Codes")
+    iface.launch(share=True)
